0edeeea70d3d45bb131a64f9987768db1d2771b6
José Manuel Nieto <superjmn@outlook.com>
2023-09-04 11:38:04 +0200

    Nukazo

--- src/Zafiro.Avalonia.FileExplorer/ViewModels/DetailsViewModel.cs
+++ src/Zafiro.Avalonia.FileExplorer/ViewModels/DetailsViewModel.cs
@@ -68,13 +68,3 @@ public class DetailsViewModel : ViewModelBase
                select f.Concat(n);
     }
 }
-
-public static class ObservableExtensions
-{
-    public static IObservable<T> DelayItem<T>(this IObservable<T> sequence, T itemToDelay, TimeSpan timeSpan) where T : notnull
-    {
-        return sequence
-            .Select(x => x.Equals(itemToDelay) ? Observable.Return(x).Delay(timeSpan) : Observable.Return(x))
-            .Switch();
-    }
-}
