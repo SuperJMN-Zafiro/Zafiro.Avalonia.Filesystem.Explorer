d5cee49207f11846b60485e929c3efe6597f18b7
José Manuel Nieto <superjmn@outlook.com>
2023-09-04 12:38:08 +0200

    First version! (#1)

---
+++ src/Zafiro.Avalonia.FileExplorer/KeyDownTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Reactive.Disposables;
+using Avalonia;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+using JetBrains.Annotations;
+
+namespace Zafiro.Avalonia.FileExplorer;
+
+[PublicAPI]
+public class KeyDownTrigger : Trigger
+{
+    public static readonly StyledProperty<RoutingStrategies> EventRoutingStrategyProperty = AvaloniaProperty.Register<KeyDownTrigger, RoutingStrategies>(nameof(EventRoutingStrategy));
+
+    public static readonly StyledProperty<Key> KeyProperty = AvaloniaProperty.Register<KeyDownTrigger, Key>(nameof(Key));
+
+    private CompositeDisposable disposables = new();
+
+    public RoutingStrategies EventRoutingStrategy
+    {
+        get => GetValue(EventRoutingStrategyProperty);
+        set => SetValue(EventRoutingStrategyProperty, value);
+    }
+
+    public Key Key
+    {
+        get => GetValue(KeyProperty);
+        set => SetValue(KeyProperty, value);
+    }
+
+    public bool MarkAsHandled { get; set; }
+
+    protected override void OnAttached()
+    {
+        if (AssociatedObject is InputElement element)
+        {
+            element
+                .AddDisposableHandler(InputElement.KeyDownEvent, OnKeyDown, EventRoutingStrategy)
+                .DisposeWith(disposables);
+        }
+    }
+
+    protected override void OnDetaching()
+    {
+        disposables.Dispose();
+    }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key)
+        {
+            e.Handled = MarkAsHandled;
+            Interaction.ExecuteActions(AssociatedObject, Actions, null);
+        }
+    }
+}
---
+++ src/Zafiro.Avalonia.FileExplorer/ViewModels/ObservableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Zafiro.Avalonia.FileExplorer.ViewModels;
+
+public static class ObservableExtensions
+{
+    public static IObservable<T> DelayItem<T>(this IObservable<T> sequence, T itemToDelay, TimeSpan timeSpan) where T : notnull
+    {
+        return sequence
+            .Select(x => x.Equals(itemToDelay) ? Observable.Return(x).Delay(timeSpan) : Observable.Return(x))
+            .Switch();
+    }
+}
