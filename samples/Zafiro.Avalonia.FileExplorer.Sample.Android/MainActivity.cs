62df343c20941becf8c0be8d8fddd12717972e9f
José Manuel Nieto <superjmn@outlook.com>
2023-12-15 00:14:35 +0100

    Android side

---
+++ samples/Zafiro.Avalonia.FileExplorer.Sample.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.ReactiveUI;
+using Zafiro.Avalonia.FileExplorer.Sample;
+using Zafiro.FileSystem.Android;
+
+namespace AvaloniaSyncer.Android;
+
+[Activity(
+    Label = "AvaloniaApplication2.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .UseReactiveUI();
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        AndroidFileSystem.Register(this);
+    }
+
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+        AndroidFileSystem.OnActivityResult(requestCode, resultCode, data);
+    }
+}
