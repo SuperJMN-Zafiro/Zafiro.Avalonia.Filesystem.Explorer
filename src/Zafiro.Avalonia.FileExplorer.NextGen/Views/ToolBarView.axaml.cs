36bf3c9acecaccd8dff27f3262e8dee7ce1d6a40
José Manuel NIeto Sánchez (SuperJMN) <superjmn@outlook.com>
2024-06-17 19:50:59 +0200

    Add ToolBarView to FileExplorer
    This commit introduces the ToolBarView UserControl to the FileExplorer project. The Toolbar provides buttons for copy, paste, delete, and select functionality. It also includes their respective commands and UI details. An isPasting status with a progress bar is also added.

---
+++ src/Zafiro.Avalonia.FileExplorer.NextGen/Views/ToolBarView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Zafiro.Avalonia.FileExplorer.NextGen.Views;
+
+public partial class ToolBarView : UserControl
+{
+    public ToolBarView()
+    {
+        InitializeComponent();
+    }
+}
